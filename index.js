0e5e15efaa4b8208795804822d04eb4c2d176899
vrkx <141280206+vrkx@users.noreply.github.com>
2025-02-19 16:16:40 -0600

    pwa

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
         window.location.href = './eilishify.html'; 
     });
 
+    self.addEventListener('install', (event) => {
+        console.log('Service Worker installing.');
+      });
+      
+      self.addEventListener('fetch', (event) => {
+        console.log('Fetching:', event.request.url);
+      });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('Service Worker Registered'))
+      .catch(err => console.log('Service Worker Failed to Register', err));
+  }
