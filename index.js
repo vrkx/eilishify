bdf96c06577f8d2e83808581c9deda80e7a50f56
vrkx <141280206+vrkx@users.noreply.github.com>
2025-02-19 19:42:40 -0600

    autoupdate

--- index.js
+++ index.js
@@ -36,3 +36,68 @@ document.addEventListener('touchend', handleTouch);
       .then(() => console.log('Service Worker Registered'))
       .catch(err => console.log('Service Worker Failed to Register', err));
   }
+
+
+  // autoupdate
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch((registrationError) => {
+          console.log('Service Worker registration failed: ', registrationError);
+        });
+    });
+  }
+
+  const CACHE_NAME = 'eilishify-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/audio',
+  '/index.js',
+  '/icon.png',
+  '/songs.json',
+  // Add other assets here
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response; // Serve from cache
+          }
+          return fetch(event.request); // Fetch from network
+        })
+    );
+  });
+
+
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+  
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName); // Delete old caches
+            }
+          })
+        );
+      })
+    );
+  });
